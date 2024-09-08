9f237dc908c69f08680766c2ea305bf290501d1d
Eboson Charles Ikenna <ebosonc@gmail.com>
2024-09-08 08:29:53 +0200

    Commit Description
    - Created amplify.yml file to handle builds manually
    - Modified FloatingButtonStyles.jsx to centralise contents of the button

--- src/helper-components/FloatingButton.jsx
+++ src/helper-components/FloatingButton.jsx
@@ -1,28 +1,5 @@
 import { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import colors from './Color';
-
-const Button = styled.button`
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  background-color: ${colors.lightBlue};
-  color: white;
-  border: none;
-  border-radius: 20%;
-  width: 50px;
-  height: 50px;
-  font-size: 24px;
-  cursor: pointer;
-  display: ${props => (props.show ? 'block' : 'none')};  
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
-  transition: opacity 0.3s ease;
-  margin-bottom: 20px;
-
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
+import { Button } from './helper-components-Styles/FloatingButtonStyles';
 
 const FloatingButton = () => {
   const [showButton, setShowButton] = useState(false);
